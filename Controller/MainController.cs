6ff31ea0d3dbf3b7d89df420d4b7f404984c5908
ElIngeniero18 <hebermelendezIS@hotmail.com>
2023-02-14 20:39:55 -0700

    Conexión BD
    Se creó la conexión y configuración a bd mediante EF

--- Controller/MainController.cs
+++ Controller/MainController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace Controller
 {
     public class MainController
     {
+        public IEnumerable<Model.OrdersModel> GetOrders() {
+
+            using (Model.service.servicioEntities db =  new Model.service.servicioEntities()) 
+            {
+                 IEnumerable < Model.OrdersModel > list = (from i in db.ordenes
+                                                  select new Model.OrdersModel
+                                                  { 
+                                                  Id= i.id,
+                                                  NombreCliente= i.nombre_cliente,
+                                                  FechaCreacion= i.fecha_creacion,  
+                                                  Descripcion= i.descripcion,
+                                                  Estado = i.estado
+                                                  }).ToList();
+                return list;
+            }
+
+        }
     }
 }
---
+++ Model/OrdersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class OrdersModel
+    {
+        public int Id { get; set; }
+        public string NombreCliente { get; set; }
+        public System.DateTime FechaCreacion { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+}
